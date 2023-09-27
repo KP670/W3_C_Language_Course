29b3bd6edbfb825a667dd7861373c842c781dad8
KPNMC2004 <143018629+KPNMC2004@users.noreply.github.com>
2023-09-27 21:12:23 +1000

    Initial Commit

---
+++ 1_01_syntax/syntax_tut.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // header library file; adds functionality to C programs
+
+int main()	// C function
+{
+	printf("Hello World!\n"); //used to output/print text to the screen, ie "Hello World!"
+	return 0; // ends the main() function
+}
+
